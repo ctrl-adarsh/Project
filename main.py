cdf8b1d8d920324912ca49087c0030ac1eccc9a6
ctrl-adarsh <83015337+ctrl-adarsh@users.noreply.github.com>
2023-01-08 18:29:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ prediction.loadModel()
 predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "image_253.png"), result_count=10)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
+    
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app.
+    app.run(host='127.0.0.1', port=8080, debug=True)
